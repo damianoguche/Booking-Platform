fd3a51ff4c457746ee32c6947038aff7f4856267
Damian Oguche <eguchealaldi@yahoo.com>
2026-02-07 07:11:35 +0100

    Updated the admin metrics

--- src/sockets/metrics.socket.js
+++ src/sockets/metrics.socket.js
@@ -5,5 +5,11 @@ module.exports = (io) => {
     socket.on("disconnect", () => {
       console.log("Admin disconnected");
     });
+
+    // Admin joins metrics room
+    socket.on("admin:join", () => {
+      socket.join("admin-dashboard");
+      console.log("Admin joined metrics room");
+    });
   });
 };
